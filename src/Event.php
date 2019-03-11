f25509ffd5000d2730575209870df12521003f8a
Derek Bourgeois <derek@devoption.io>
2019-03-11 00:02:59 -0400

    add table name to model

--- src/Event.php
+++ src/Event.php
@@ -7,6 +7,13 @@ use Illuminate\Database\Eloquent\Model;
 class Event extends Model
 {
     /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'event_tracking';
+
+    /**
      * The attributes that are mass assignable.
      *
      * @var array
